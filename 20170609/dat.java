3c9af82aaf8c9b7da3674ba361f1e22431f05d25
LuQifan <44954915+learninger-lu@users.noreply.github.com>
2018-11-13 22:39:19 +0800

    Add files via upload

---
+++ 20170609/dat.java
@@ -0,0 +1,16 @@
+public class dat 
+{
+	
+	public static void main(String[] args)
+	{
+		int[] intarr;
+		intarr= new int[]{5,6,7,8};
+		Object[]  objectar;//Object 是一个包含所有变量类型的类
+		objectar = new String[]{"java","bb"};
+		Object[]  obj2;
+		obj2 = new Object[]{"java","bong"};
+ 		
+		System.out.println(obj2[1]);
+		System.out.println(objectar[1]);
+	}
+}
